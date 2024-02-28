b166109c564a7b46f42f8c09f2f5e27f8ac0c8da
Theodor <71594408+AmpasTheodoros@users.noreply.github.com>
2024-02-29 01:11:24 +0200

    Folder Setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <h1>Landing Page (Protected)</h1>
+        </div>
+    )
+}
+
+export default DashboardPage;
