51bd4d34a6f67c895ef8137c379f898d07a43cea
Theodor <71594408+AmpasTheodoros@users.noreply.github.com>
2024-02-29 02:16:16 +0200

    Clerk Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
     return (
         <div>
             <h1>Landing Page (Protected)</h1>
+            <UserButton afterSignOutUrl="/"></UserButton>
         </div>
     )
 }
